42bbe964e3ba1b3d2295b206cb48d01d91a6c17b
Keerthana-30 <71180623+Keerthana-30@users.noreply.github.com>
2022-04-21 00:13:04 +0530

    Add files via upload

---
+++ Lamp/python/tryout.py
@@ -0,0 +1,32 @@
+# s=input()
+# subst=input()
+# print(s.count(subst))
+
+
+# star pattern
+# for i in range(9):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print("\n")
+
+
+# n1=int(input())
+# n2=int(input())
+# print(str(n2))
+# if(n1 == int(str(n2)[::-1])):
+#     print("yes")
+# else:
+#     print("no")
+
+
+# l1 = list(map(int,input().strip().split()))
+# l2 = list(map(int,input().strip().split()))
+# l3=[]
+# print(l1,l2)
+# for i in l1:
+#     if(i%2==0):
+#         l3.append(i)
+# print(l3)
+
+# n= int(input())
+# print(str(n)[::-1])
