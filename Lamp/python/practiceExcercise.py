7c2cbbe07614f3a6a3b43a3fa12f1c6ee3ed46ac
Keerthana-30 <71180623+Keerthana-30@users.noreply.github.com>
2022-04-28 10:37:28 +0530

    Add files via upload

---
+++ Lamp/python/practiceExcercise.py
@@ -0,0 +1,48 @@
+"""firstName,lastName=input().split(" ")#kiki koko
+print(lastName,firstName)#koko kiki
+print(lastName[::-1],firstName[::-1])#printing names in reverse order
+print(firstName*2)#kikikiki
+print(firstName[:3])#kik
+
+print(list(firstName))
+
+#assert
+a = 10  
+b = 0  
+print('a is dividing by Zero')  
+print(a/b)"""
+
+# #changing to tuple
+# values = input().split(",")
+# print(values)
+# print(type(values))
+# print(tuple(values))
+
+# filename = input()
+# print(filename.split('.')[-1])#splits with the '.' and prints the last value
+
+# a=int(input())
+# b=int(input())
+# pdt=a*b
+# if pdt>100 :
+#     print(pdt)
+# else:
+#     print("small")
+
+# the sum of the current and previous number.
+# a=int(input())
+# for i in range(1,a):
+#     print("%d + %d = %d" %(i,i-1,i+(i-1)))
+
+# display characters that are present at an even index number.
+# s=input()
+# print(len(s))
+# for i in range(0,len(s),2):
+#     print(s[i],end=" ")
+#     (or)
+# print(s[::2])
+
+# s=input()
+# n=int(input())
+# print(s[n:])
+
---
+++ Lamp/python/shel.py
@@ -0,0 +1,62 @@
+
+# l1 = list()
+# print(l1)
+# l2 = (2,5,7)
+# l1.append([10,90]) #10 and 90 will be appended as a single element [10,90]
+# l1.extend([9,0,7]) # 9,0,7 will be added as seperate element 9,0,7
+# l1.append(l2)      # (2,5,7) as a single item ... subtuple
+# print(l1)
+# l1.extend(l2)      #2,5,7 as a seperate element
+# print(l1)
+# l1.append(0)
+# l1.insert(2,[8,9123])
+# print(l1)
+# l1.pop() #removes the last if index is not mentioned else removes the indexed item
+# print(l1)
+
+l1 = list()
+print(l1)
+l2 = (2,5,7)
+l1.append([10,90]) #10 and 90 will be appended as a single element [10,90]
+l1.extend([9,0,7]) # 9,0,7 will be added as seperate element 9,0,7
+l1.append(l2)      # (2,5,7) as a single item ... subtuple
+print(l1)
+l1.extend(l2)      #2,5,7 as a seperate element
+print(l1)
+l1.append(0)
+l1.insert(2,[8,9123])
+print(l1)
+l1.pop() #removes the last if index is not mentioned else removes the indexed item
+print(l1)
+
+
+# l2.sort() # sort is only for list
+l3=[90,76786,821,0]
+print(l3.sort()) # changes in place and returns None
+print(l3)
+l3.sort(reverse=True)
+print(l3)
+print(sorted(l3)) #for all iterables
+
+
+student_tuples = [('john', 'A', 15),('jane', 'B', 12),('dave', 'B', 10)]
+print(sorted(student_tuples, key=lambda student: student[2]))   # sort by age
+
+
+# # l2.sort() # sort is only for list
+# l3=[90,76786,821,0]
+# print(l3.sort()) # changes in place and returns None
+# print(l3)
+# l3.sort(reverse=True)
+# print(l3)
+# print(sorted(l3)) #for all iterables
+
+
+# student_tuples = [('john', 'A', 15),('jane', 'B', 12),('dave', 'B', 10)]
+# print(sorted(student_tuples, key=lambda student: student[2]))   # sort by age
+
+
+
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+for name, phone in table.items():
+    print(f'{name:6} ==> {phone:1d}')
